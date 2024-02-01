b08aa73aa98a1c7f318a2b4cd2ad2084ac2c9fe8
José Aguilar <josehollow24@gmail.com>
2024-02-01 00:57:21 -0300

    add i18n

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import i18n from '@/utils/i18n.js'
 
 export default function Home() {
+
   return (
     <main >
-      Aplicacion principal uwu
+      <h1>{i18n.t('appTitle')}</h1>
     </main>
   );
 }
---
+++ src/utils/i18n.js
@@ -0,0 +1,29 @@
+import i18next from 'i18next';
+
+i18next.init({
+  lng: 'es', // if you're using a language detector, do not define the lng option
+  debug: true,
+  resources: {
+    en: {
+        translation: {
+          appTitle: 'Pokedex App',
+          pokemondex: 'Pokemon Pokedex',
+          cardsdex: 'Cards Pokedex',
+          about: 'About',
+          // ... otras traducciones en inglés
+        },
+      },
+      es: {
+        translation: {
+          appTitle: 'Aplicación Pokedex',
+          pokemondex: 'Pokedex de Pokémon',
+          cardsdex: 'Pokedex de Cartas',
+          about: 'Acerca de',
+          // ... otras traducciones en español
+        },
+      },
+  }
+});
+// initialized and ready to go!
+// i18next is already initialized, because the translation resources where passed via init function
+export default i18next;
