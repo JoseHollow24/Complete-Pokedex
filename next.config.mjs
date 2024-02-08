1a2c2ddaa8a6975eeaff06bbd95034b5714827ac
José Aguilar <josehollow24@gmail.com>
2024-02-07 23:39:19 -0300

    update next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        loader: 'default',
         domains: ['images.pokemontcg.io', 'raw.githubusercontent.com'], // Agrega el dominio de tu host aquí
       },
 };
